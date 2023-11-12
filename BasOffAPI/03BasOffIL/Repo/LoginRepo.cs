2e105d504f1c2e0b1271aa3e5b1e45ad7ab2168e
William Slabbaert <56199868+WilliamSlabbaert@users.noreply.github.com>
2023-11-12 18:08:47 +0100

    folder rename

---
+++ BasOffAPI/03BasOffIL/Repo/LoginRepo.cs
@@ -0,0 +1,13 @@
+﻿using _02BasOffBL.Repo;
+
+
+namespace _01BasOffDL.Repo
+{
+    public class LoginRepo : ILoginRepo
+    {
+        public string Login()
+        {
+            return "hello from repo";
+        }
+    }
+}
